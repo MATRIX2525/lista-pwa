c660659860ea44780ebbab082de5b508360a7630
MATRIX2525 <producaostudio764@gmail.com>
2026-01-03 22:06:04 -0300

    Add files via upload
    Lista de Compras

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,14 @@
 const CACHE_NAME = 'lista-pwa-cache-v1';
-const urlsToCache = ['/', '/index.html', '/icon-512.png', '/icon-192.png'];
+const urlsToCache = ['/', '/index.html', '/icon-512.png', '/icon-192.png', '/style.css', '/app.js'];
+
 self.addEventListener('install', (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
 });
+
 self.addEventListener('fetch', (event) => {
-  event.respondWith(caches.match(event.request).then((response) => response || fetch(event.request)));
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
 });
